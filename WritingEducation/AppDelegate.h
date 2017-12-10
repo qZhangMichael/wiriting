17fae4cfc590ffbf003b88c40fcc56adbeb07549
qzhangMichael <qzhangXCX@163.com>
2017-12-10 22:20:49 +0800

    Initial Commit

---
+++ WritingEducation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WritingEducation
+//
+//  Created by 张琼 on 2017/12/10.
+//  Copyright © 2017年 qzhangmichael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WritingEducation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WritingEducation
+//
+//  Created by 张琼 on 2017/12/10.
+//  Copyright © 2017年 qzhangmichael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
